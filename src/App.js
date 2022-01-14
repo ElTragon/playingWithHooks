e9387cbfdd0ba9bfc2d43139f6ffdeff14dba738
Mario L <m9lopez@ucsd.edu>
2022-01-13 18:20:37 -0800

    Made useRef to get prev state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UseRefExample_1 from './components/UseRefExample_1';
+import UseRefExample_2 from './components/UseRefExample_2';
 
 function App() {
   return (
     <div className='conatainer mt-5'>
       <h1>Hello</h1>
       <UseRefExample_1 />
+      <UseRefExample_2 />
     </div>
   );
 }
