2e178996852ffa107ddf13d8064b7c1f21a7855d
Mario L <m9lopez@ucsd.edu>
2022-01-23 20:01:07 -0800

    useLocalStoraghook

--- src/components/CustomUseLocalStorageHook.jsx
+++ src/components/CustomUseLocalStorageHook.jsx
@@ -1,11 +1,49 @@
-import { useState } from "react";
-
+// import { useState } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function CustomUseLocalStorageHook() {
-    const [task, setTask] = useState('')
-  return <div>
-      soemthing
-  </div>;
+  //   const [task, setTask] = useState('');
+  const [task, setTask] = useLocalStorage('task', ''); // single value
+  const [tasks, setTasks] = useLocalStorage('tasks', []); // array of values
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('ssssssssss')
+
+    const taskObj = {
+      task,
+      completed: false,
+      date: new Date().toLocaleDateString(),
+    };
+
+    setTasks([...tasks, taskObj]);
+  };
+
+  return (
+      <>
+    <form className='w-25' onSubmit={onSubmit}>
+      <div className='mb-3'>
+        <label className='form-label'>Task</label>
+        <input
+          type='text'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+      </div>
+
+      <button className='btn btn-primary' type='submit'>
+        Submit
+      </button>
+    </form>
+    
+    <hr />
+
+    {tasks.map((task) => (
+        <h3 key={task.date}>{task.task}</h3>
+    ))}
+    
+    </>
+  );
 }
 
 export default CustomUseLocalStorageHook;
